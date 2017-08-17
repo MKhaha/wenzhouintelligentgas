ff6a27047ff2bfa1a3dd5a43d593c98da6ce0b79
guotao <xieguotao@ywgroup.com.cn>
2017-08-17 20:44:06 +0800

    Initial commit

---
+++ src/main/java/com/ywGroup/ieCloud/wenZhouIntelligentGas/common/ResponseCode.java
@@ -0,0 +1,32 @@
+package com.ywGroup.ieCloud.wenZhouIntelligentGas.common;
+
+/**
+ * Created by geely
+ */
+public enum ResponseCode {
+
+    SUCCESS(0,"SUCCESS"),
+    ERROR(1,"ERROR"),
+    NEED_LOGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT"),
+    REPEAT_OPERATION(3,"REPEAT_OPERATION");
+
+
+
+    private final int code;
+    private final String desc;
+
+
+    ResponseCode(int code,String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode(){
+        return code;
+    }
+    public String getDesc(){
+        return desc;
+    }
+
+}
